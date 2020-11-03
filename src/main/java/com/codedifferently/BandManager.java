13493c077873ae6eb7d4d9f312e982d5a86383b1
mehCodeGroup <iyasuwatts@Iyasus-MacBook-Air.local>
2020-11-02 21:00:04 -0500

    comp

--- src/main/java/com/codedifferently/BandManager.java
+++ src/main/java/com/codedifferently/BandManager.java
@@ -8,17 +8,14 @@ public class BandManager {
         this.wuMemberArray = wuMemberArray;
     }
 
+    int counter = 0;
     public String whileLoop() {
         String result = "";
-        // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
+        while (counter < wuMemberArray.length){
+            result += wuMemberArray[counter].toString();
+            counter++;
+        }
+        
         return result;
     }
 
@@ -26,17 +23,9 @@ public class BandManager {
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for (int i = 0; i < wuMemberArray.length; i++) {
+            result += wuMemberArray[i];
+        }
         return result;
     }
 
@@ -44,15 +33,9 @@ public class BandManager {
 
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for ( WuMember w: wuMemberArray) {
+            result += w.toString();
+        }
         return result;
     }
 
